8c23f216203356ab7e6f2247b41fe4101573fa44
Joas329 <joaquinphilco@hotmail.com>
2026-01-23 16:14:56 -0500

    trewide: Add star background.
    Signed-off-by: Joas329 <joaquinphilco@hotmail.com>

--- portafolio-joaquin/src/pages/Home.jsx
+++ portafolio-joaquin/src/pages/Home.jsx
@@ -1,11 +1,12 @@
-import { ThemeToggle } from "../components/ThemeToggle"
+import { ThemeToggle } from "@/components/ThemeToggle"
+import { Background } from "@/components/Background"
 
 export const Home = () => {
     return <div className="min-h-screen bg-background  text-foreground overflow-x-hidden">
             {/* Theme Toggle*/}
                 <ThemeToggle />
             {/* Background Effects*/}
-
+                <Background />
             {/* Navbar*/}
 
             {/* Main Content*/}
