878877e592156681c7d2675c07a4aa9d234df5a2
Joas329 <joaquinphilco@hotmail.com>
2026-01-24 20:05:49 -0500

    Reduce amoutn of background moving and radomize initial spawn points.
    Signed-off-by: Joas329 <joaquinphilco@hotmail.com>

--- portafolio-joaquin/src/components/Background.jsx
+++ portafolio-joaquin/src/components/Background.jsx
@@ -9,15 +9,16 @@ export const Background = () => {
         generateStarts();
         generateMeteors();
 
-        const handleResize = () => {
-            generateStarts();
-        };
+        const meteorInterval = setInterval(() => {
+            generateMeteors();
+        }, 4000);
 
-        window.addEventListener('resize', handleResize);
+        window.addEventListener("resize", generateStarts);
 
         return () => {
-            window.removeEventListener('resize', handleResize);
-        }
+            clearInterval(meteorInterval);
+            window.removeEventListener("resize", generateStarts);
+        };
     }, []);
 
     const generateStarts = (num) => {
@@ -42,19 +43,28 @@ export const Background = () => {
     }
 
     const generateMeteors = () => {
-        const numberOfMeteors = 10;
-        const newMeteors = [];
+        const numberOfMeteors = 4;
 
-        for (let i = 0; i < numberOfMeteors; i++) {
-            newMeteors.push({
-            id: i,
-            size: Math.random() * 3 + 1,
-            x: Math.random() * 100,
-            y: Math.random() * 100,
-            delay: Math.random() * 15,
-            animationDuration: Math.random() * 3 + 3,
-            });
-        }
+        const newMeteors = Array.from({ length: numberOfMeteors }, () => {
+            const fromTop = Math.random() < 0.5;
+
+            const x = fromTop
+            ? Math.random() * window.innerWidth
+            : -100 - Math.random() * 200; // start left of screen
+
+            const y = fromTop
+            ? -100 - Math.random() * 200 // start above screen
+            : Math.random() * window.innerHeight;
+
+            return {
+            id: crypto.randomUUID(),
+            size: Math.random() * 2 + 1,
+            x,
+            y,
+            delay: Math.random() * 2,
+            animationDuration: Math.random() * 2 + 2,
+            };
+        });
 
         setMeteors(newMeteors);
     };
@@ -77,16 +87,16 @@ export const Background = () => {
             ))}
 
             {meteor.map((meteor) => (
-                <div 
+                <div
                     key={meteor.id}
                     className="meteor animate-meteor"
                     style={{
-                        width: meteor.size * 20 + "px",
-                        height: meteor.size + "px",
-                        left: meteor.x + "%",
-                        top: meteor.y + "%",
-                        animationDelay: meteor.delay,
-                        animationDuration: meteor.animationDuration + "s",
+                    width: meteor.size * 40 + "px",
+                    height: meteor.size + "px",
+                    left: meteor.x + "px",
+                    top: meteor.y + "px",
+                    animationDelay: meteor.delay + "s",
+                    animationDuration: meteor.animationDuration + "s",
                     }}
                 />
             ))}
