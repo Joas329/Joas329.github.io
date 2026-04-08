c948d5a8ed306bc9d32c97ed6ab079621c1c5651
Joas329 <joaquinphilco@hotmail.com>
2026-04-08 03:05:09 -0400

    routes: Add phantom route to Transcripts.
    Signed-off-by: Joas329 <joaquinphilco@hotmail.com>

--- portafolio-joaquin/src/App.jsx
+++ portafolio-joaquin/src/App.jsx
@@ -1,6 +1,7 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
 import { Projects } from "./pages/ProjectsPage";
 import { NotFound } from "./pages/NotFound";
+import { Transcripts } from "./pages/Transcripts";
 import { Home } from "./pages/Home";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="*" element={<NotFound />} />
+        <Route path="/transcripts" element={<Transcripts />} />
       </Routes>
     </HashRouter>
   );
